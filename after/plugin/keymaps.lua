37df3a185b08be625bea92c5c0e2adfaba451e69
XenophonLXH <lourenzo321@gmail.com>
2023-02-13 23:16:02 +0200

    [M] keymaps: Added some basic file management keymaps

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -12,9 +12,19 @@ keymap("n", "<Down>", ":resize +1<CR>", default_opts)
 
 -- Neotree
 keymap("n", "<C-o>", ":Neotree left reveal_force_cwd<cr>", default_opts) -- focus tree and reveal current file
-keymap("n", "<S-o>", ":Neotree close<cr>", default_opts) -- close tree
-keymap("n", "<C-g>", ":Neotree toggle float git_status<cr>", default_opts) -- Show git status float
+keymap("n", "<C-e>", ":Neotree toggle<cr>", default_opts) -- close tree keymap("n", "<C-g>", ":Neotree toggle float git_status<cr>", default_opts) -- Show git status float
 
 
 -- ToggleTerm
 keymap("n", "<C-t>", ":ToggleTerm size=5 direction=float<cr>", default_opts)
+keymap("n", "<F7>", "<cmd>ToggleTerm<cr>", default_opts)
+
+-- Buffer Navigation
+keymap("n", "<S-l>", "<cmd>bnext<cr>", default_opts)
+keymap("n", "<S-h>", "<cmd>bprevious<cr>", default_opts)
+
+-- File Operations
+keymap("n", "<C-s>", "<cmd>w<cr>", default_opts)
+keymap("n", "<C-q>", "<cmd>q<cr><cr>", default_opts)
+keymap("n", "|", "<cmd>vsplit<cr>", default_opts)
+keymap("n", "\\", "<cmd>split<cr>", default_opts)
