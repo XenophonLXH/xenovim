9a75cab59c726b795d4045983966559b2ac37d54
XenophonLXH <lourenzo321@gmail.com>
2023-02-14 21:19:16 +0200

    [A] harpoon: Added initial harpoon mappings

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -2,19 +2,16 @@ local keymap = vim.api.nvim_set_keymap
 local default_opts = { noremap = true, silent = true }
 local expr_opts = { noremap = true, expr = true, silent = true }
 
-
 -- Resizing panes
-keymap("n", "<Left>", ":vertical resize +1<CR>", default_opts)
-keymap("n", "<Right>", ":vertical resize -1<CR>", default_opts)
+keymap("n", "<C-Left>", ":vertical resize +1<CR>", default_opts)
+keymap("n", "<C-Right>", ":vertical resize -1<CR>", default_opts)
 keymap("n", "<Up>", ":resize -1<CR>", default_opts)
 keymap("n", "<Down>", ":resize +1<CR>", default_opts)
-
 
 -- Neotree
 keymap("n", "<C-o>", ":Neotree left reveal_force_cwd<cr>", default_opts) -- focus tree and reveal current file
 keymap("n", "<C-e>", ":Neotree toggle<cr>", default_opts) -- close tree 
 keymap("n", "<C-g>", ":Neotree toggle float git_status<cr>", default_opts) -- Show git status float
-
 
 -- ToggleTerm
 keymap("n", "<C-t>", ":ToggleTerm size=5 direction=float<cr>", default_opts)
