44013e0b2c8e64e515a6bcbfb1f20a040d6f29c0
XenophonLXH <lourenzo321@gmail.com>
2024-04-14 21:03:35 +0200

    [IMP] nvim-dap-ui: Removal of unused elements

--- lua/xenophon/dap_ui.lua
+++ lua/xenophon/dap_ui.lua
@@ -42,19 +42,24 @@ dapui.setup({
       expanded = ""
     },
     layouts = { {
-        elements = { {
+        elements = {
+        {
             id = "scopes",
-            size = 0.25
-          }, {
+            size = 0.5
+          },
+          {
             id = "breakpoints",
             size = 0.25
-          }, {
+          },
+          {
             id = "stacks",
             size = 0.25
-          }, {
-            id = "watches",
-            size = 0.25
-          } },
+          },
+          -- {
+          --   id = "watches",
+          --   size = 0.25
+          -- } 
+      },
         position = "right",
         size = 40
       }, {
