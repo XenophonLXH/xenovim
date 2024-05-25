868e49aabddd512c0697947b81faeccd20da3527
XenophonLXH <lourenzo321@gmail.com>
2024-05-25 12:06:09 +0200

    [IMP] toggleterm: Changed size constraints on terminal]

--- lua/xenophon/toggleterm.lua
+++ lua/xenophon/toggleterm.lua
@@ -2,8 +2,8 @@ require("toggleterm").setup({
     autochdir = false,
     size = 1,
     float_opts = {
-        width = 180,
-        height = 100,
+        width = 160,
+        height = 60,
         winblend = 5,
         border = 'curved',
     },
