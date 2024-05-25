8c3b4d6b537eae51725170b21ecc100fd5e7005e
XenophonLXH <lourenzo321@gmail.com>
2024-05-25 11:53:46 +0200

    [IMP] telescope: Changed the way I display telescope results

--- lua/xenophon/telescope.lua
+++ lua/xenophon/telescope.lua
@@ -1,9 +1,44 @@
 require('telescope').setup{ 
-  defaults = { 
-    file_ignore_patterns = { 
+  defaults = {
+    layout_config = {
+      width = 0.9,
+      height = 200,
+      bottom_pane = {
+        height = 25,
+        preview_cutoff = 120,
+        prompt_position = "top"
+      },
+      center = {
+        height = 0.4,
+        preview_cutoff = 40,
+        prompt_position = "top",
+        width = 0.5
+      },
+      cursor = {
+        height = 0.9,
+        preview_cutoff = 40,
+        width = 0.8
+      },
+      horizontal = {
+        height = 0.9,
+        preview_cutoff = 120,
+        prompt_position = "bottom",
+        width = 0.8
+      },
+      vertical = {
+        anchor = "center",
+        height = 0.99,
+        preview_height = 0.8,
+        preview_cutoff = 40,
+        prompt_position = "top",
+        width = 0.9
+      }
+    },
+    layout_strategy = "vertical",
+    file_ignore_patterns = {
       "node_modules",
       "*.po",
       "%.po",
-    }
-  }
+    },
+  },
 }
