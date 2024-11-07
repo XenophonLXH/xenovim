6d11af56f2a56d698db1f72e5c003342d4ce14f3
XenophonLXH <lourenzo321@gmail.com>
2024-11-07 13:23:14 +0200

    [IMP] Updated theme

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -18,8 +18,12 @@ lsp.configure('pylsp', {
           },
         },
       },
+    root_dir = "/home/xenophon/Development/"
 })
 
+lsp.configure('ts_ls', {
+    root_dir = "/home/xenophon/Development/"
+})
 -- Fix Undefined global 'vim'
 lsp.configure('lua-language-server', {
     settings = {
@@ -28,7 +32,8 @@ lsp.configure('lua-language-server', {
                 globals = { 'vim' }
             }
         }
-    }
+    },
+    root_dir = "/home/xenophon/Development/"
 })
 
 
@@ -36,7 +41,12 @@ lsp.configure('lua-language-server', {
 lsp.configure('rust-analyzer', {
     diagnostics = {
         enabled = true;
-    }
+    },
+    root_dir = "/home/xenophon/Development/"
+})
+
+lsp.configure('ruff_lsp', {
+    root_dir = "/home/xenophon/Development/"
 })
 
 
@@ -70,7 +80,7 @@ lsp.on_attach(function(client, bufnr)
   local opts = {buffer = bufnr, remap = false}
 
   vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-  vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+  vim.keymap.set("n", "K", function(args) vim.lsp.buf.hover() end, { buffer = args.buf})
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
   vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
--- lua/xenophon/theme.lua
+++ lua/xenophon/theme.lua
@@ -8,7 +8,8 @@ vim.api.nvim_set_hl(0, "Normal", { bg = "none"})
 vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
 
 require("monokai-pro").setup({
-  transparent_background = true,
+  -- transparent_background = true,
+  transparent_background = false,
   terminal_colors = true,
   devicons = true, -- highlight the icons of `nvim-web-devicons`
   styles = {
@@ -21,11 +22,11 @@ require("monokai-pro").setup({
     annotation = { italic = true },
     tag_attribute = { italic = true }, -- attribute of tag in reactjs
   },
-  filter = "classic", -- classic | octagon | pro | machine | ristretto | spectrum
+  filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
   -- Enable this will disable filter option
   day_night = {
     enable = true, -- turn off by default
-    day_filter = "classic", -- classic | octagon | pro | machine | ristretto | spectrum
+    day_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
     night_filter = "classic", -- classic | octagon | pro | machine | ristretto | spectrum
   },
   inc_search = "background", -- underline | background
@@ -49,7 +50,36 @@ require("monokai-pro").setup({
       context_highlight = "default", -- default | pro
       context_start_underline = false,
     },
-  },})
+  },
+  overridePalette = function(filter)
+      return {
+        dark2 = "#101014",
+        dark1 = "#16161E",
+        background = "#1A1B26",
+        text = "#C0CAF5",
+        accent1 = "#f7768e",
+        accent2 = "#7aa2f7",
+        accent3 = "#e0af68",
+        accent4 = "#9ece6a",
+        accent5 = "#0DB9D7",
+        accent6 = "#9d7cd8",
+        dimmed1 = "#737aa2",
+        dimmed2 = "#787c99",
+        dimmed3 = "#363b54",
+        dimmed4 = "#363b54",
+        dimmed5 = "#16161e",
+      }
+    end,
+  overrideScheme = function(cs, p, config, hp)
+      local cs_override = {}
+      local calc_bg = hp.blend(p.background, 0.5, '#000000')
+
+      cs_override.editor = {
+        background = calc_bg,
+      }
+      return cs_override
+    end
+  })
 -- lua
 vim.cmd([[colorscheme monokai-pro]])
 vim.cmd([[hi Cursor guibg=red]])
