9bf8f7b8f71103313b2e8e4fbbbb3004ff00290a
XenophonLXH <lourenzo321@gmail.com>
2025-07-26 18:50:43 +0200

    [FIX] notifications

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,9 +1,10 @@
 return {
     "rcarriga/nvim-notify",
     config = function(_, opts)
-        vim.notify = require('notify').setup(vim.tbl_extend('keep', {
+        require('notify').setup(vim.tbl_extend('keep', {
             -- other stuff
             background_colour = "#000000"
         }, opts))
+        vim.notify = require('notify')
     end
 }
