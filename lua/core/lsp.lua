7398d854f37d3ed929e21ac2eed86356a95aaa66
XenophonLXH <lourenzo321@gmail.com>
2025-07-26 12:52:05 +0200

    [IMP] lsp: Added the required lsp files

--- lua/core/lsp.lua
+++ lua/core/lsp.lua
@@ -1,9 +1,14 @@
 vim.lsp.enable({
+    'lua_ls',
+    'odoo_lsp',
+    'pylsp',
+    'rust_analyzer',
+    'ts_ls',
 })
 
 vim.diagnostic.config({
     virtual_lines = true,
-    -- virtual_text = true,
+    virtual_text = true,
     underline = true,
     update_in_insert = false,
     severity_sort = true,
