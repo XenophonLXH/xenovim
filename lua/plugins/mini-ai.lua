13a59bad17c1eb3240c17f3a1ae3c9e8ea3b8535
XenophonLXH <lourenzo321@gmail.com>
2025-07-30 20:57:47 +0200

    [FIX] Added m as motion for previous with mini-ai selection

--- lua/plugins/mini-ai.lua
+++ lua/plugins/mini-ai.lua
@@ -7,6 +7,23 @@ return {
             -- Function definition (needs treesitter queries with these captures)
             F = gen_spec.treesitter({ a = '@function.outer', i = '@function.inner' }),
             },
+            mappings = {
+                -- Main textobject prefixes
+                around = 'a',
+                inside = 'i',
+
+                -- Next/last textobjects
+                -- NOTE: These override built-in LSP selection mappings on Neovim>=0.12
+                -- Map LSP selection manually to use it (see `:h MiniAi.config`)
+                around_next = 'an',
+                inside_next = 'in',
+                around_last = 'am',
+                inside_last = 'im',
+
+                -- Move cursor to corresponding edge of `a` textobject
+                goto_left = 'g[',
+                goto_right = 'g]',
+            },
 
             n_lines = 500,
         })
