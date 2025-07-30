4049e337246933fece558526f00a2e5020f36294
XenophonLXH <lourenzo321@gmail.com>
2025-07-30 09:58:16 +0200

    [FIX] lualine: Used ignore focus to prevent jumping

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -17,10 +17,10 @@ return {
                     tabline = 1000,
                     winbar = 1000,
                 },
-                disabled_filetypes = {
-                    'dashboard', 'alpha', 'NvimTree', 'Outline', 'toggleterm',
-                    'Trouble', 'lazy', 'mason', 'help', 'TelescopePrompt',
-                    'NeoTree', 'neo-tree', 'NeogitStatus'
+                ignore_focus = {
+                    'NeoTree',
+                    'neo-tree',
+                    'NeogitStatus'
                 },
             },
             sections = {
