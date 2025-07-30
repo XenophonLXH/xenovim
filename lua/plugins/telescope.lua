4529a371672708522d09f35440d835bd33312097
XenophonLXH <lourenzo321@gmail.com>
2025-07-30 08:36:10 +0200

    [FIX] telescope: Updated version for telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,6 +1,5 @@
 return {
     'nvim-telescope/telescope.nvim',
-    version = '0.1.4',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
         require('telescope').setup({
