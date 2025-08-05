f967a96531bcfe61707d9a2fdfc73c0864079759
XenophonLXH <lourenzo321@gmail.com>
2025-08-05 21:44:24 +0200

    [IMP] Updated the keymaps to be used in oil.nvim

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -6,11 +6,18 @@ return {
     config = function()
         require("oil").setup({
             default_file_explorer = false,
+            use_default_keymaps = false,
             view_options = {
                 show_hidden = true, -- Show hidden files
                 show_parent_dir = true, -- Show parent directory
             },
             keymaps = {
+                ["<CR>"] = "actions.select",
+                ["<C-c>"] = { "actions.close", mode = "n" },
+                ["P"] = "actions.preview",
+                ["-"] = { "actions.parent", mode = "n" },
+                ["_"] = { "actions.open_cwd", mode = "n" },
+                ["."] = { "actions.cd", mode = "n" },
             },
         })
         vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
