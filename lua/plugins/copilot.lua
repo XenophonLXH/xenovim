a472300243e484f589fa738e91407eba0b759d9d
XenophonLXH <lourenzo321@gmail.com>
2025-09-10 15:06:00 +0200

    [IMP] disable copilot by default, enabled by <leader>cee

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,5 +2,7 @@ return {
     "github/copilot.vim",
     config = function()
         vim.g.copilot_workspace_folders = {"~/Development/"}
+        -- Disable copilot by default
+        vim.cmd("Copilot disable")
     end
 }
