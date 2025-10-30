99386951ca5a3e8820ace318190623c79bd93263
XenophonLXH <lourenzo321@gmail.com>
2025-10-30 11:25:14 +0200

    [IMP] Added leap-from-window with gs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -122,5 +122,6 @@ vim.keymap.set("n", "<leader>xl", function()
 end, { desc = "Location List" })
 
 -- nvim.leap
-vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap)')
-vim.keymap.set('n',             'S', '<Plug>(leap-from-window)')
+vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap-forward)')
+vim.keymap.set('n',             'S', '<Plug>(leap-backward)')
+vim.keymap.set('n',             'gs', '<Plug>(leap-from-window)')
