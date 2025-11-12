2bfb2e6d4d9f45e8ae651a2a5e84aa2ce8d74447
Xenophon <lourenzo321@gmail.com>
2025-11-12 18:47:50 +0200

    [IMP] Added script dir

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 rm -rf ~/.config/nvim/
-cp -r ./ ~/.config/nvim
+cp -r $SCRIPT_DIR ~/.config/nvim
 echo "Xenovim installation complete...."
