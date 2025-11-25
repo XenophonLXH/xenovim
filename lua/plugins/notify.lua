848b49316630d2c853732b4c99542192dc6ace5e
Xenophon <lourenzo321@gmail.com>
2025-11-25 14:41:26 +0200

    {FIX} notify FPS

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -3,7 +3,8 @@ return {
     config = function(_, opts)
         require('notify').setup(vim.tbl_extend('keep', {
             -- other stuff
-            background_colour = "#000000"
+            background_colour = "#000000",
+            fps = 144
         }, opts))
         vim.notify = require('notify')
     end
