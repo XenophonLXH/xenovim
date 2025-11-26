81c1073af91a780a2de2f52eab9720326bcb8ff5
Xenophon <lourenzo321@gmail.com>
2025-11-26 09:55:44 +0200

    {REF} Refactored nvim-notify for better frames and animation

--- lua/plugins/notify.lua
+++ lua/plugins/notify.lua
@@ -1,11 +1,28 @@
 return {
-    "rcarriga/nvim-notify",
-    config = function(_, opts)
-        require('notify').setup(vim.tbl_extend('keep', {
-            -- other stuff
-            background_colour = "#000000",
-            fps = 144
-        }, opts))
-        vim.notify = require('notify')
-    end
+  "rcarriga/nvim-notify",
+  opts = {
+    fps = 144,
+    icons = {
+      DEBUG = "",
+      ERROR = "",
+      INFO = "",
+      TRACE = "✎",
+      WARN = ""
+    },
+    background_colour = "#000000",
+    level = 2,
+    minimum_width = 50,
+    render = "default",
+    stages = "static",
+    time_formats = {
+      notification = "%T",
+      notification_history = "%FT%T"
+    },
+    timeout = 5000,
+    top_down = true
+  },
+  config = function(_, opts)
+    vim.notify = require("notify").setup(opts)
+    vim.notify = require('notify')
+  end,
 }
