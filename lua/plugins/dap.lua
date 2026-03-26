2b2f8a6b9d945c2f89f23913b251b59cfaa6eea6
XenophonLXH <lourenzo321@gmail.com>
2026-03-26 14:32:46 +0200

    {IMP} Pretty break points

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -7,5 +7,11 @@ return {
     },
     config = function()
         require('dap-python').setup('/home/xenophon/Development/.virtualenvs/debugpy/bin/python')
+
+        vim.fn.sign_define('DapBreakpoint',          { text = '●', texthl = 'DiagnosticError', linehl = '', numhl = '' })
+        vim.fn.sign_define('DapBreakpointCondition', { text = '◆', texthl = 'DiagnosticWarn',  linehl = '', numhl = '' })
+        vim.fn.sign_define('DapBreakpointRejected',  { text = '◯', texthl = 'DiagnosticHint',  linehl = '', numhl = '' })
+        vim.fn.sign_define('DapLogPoint',            { text = '◆', texthl = 'DiagnosticInfo',  linehl = '', numhl = '' })
+        vim.fn.sign_define('DapStopped',             { text = '▶', texthl = 'DiagnosticOk',    linehl = 'debugPC', numhl = '' })
     end,
 }
